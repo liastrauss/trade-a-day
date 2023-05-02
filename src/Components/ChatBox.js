3a16d7506f4648ebe428898d4a9f9b29f5b53936
yuvalshayo <yuval.shayo@gmail.com>
2023-05-02 22:36:43 +0300

    adjusting chat still working in changing topbar to appbar type so it will be sticky and elevated

--- src/Components/ChatBox.js
+++ src/Components/ChatBox.js
@@ -3,42 +3,26 @@ import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import SendIcon from '@mui/icons-material/Send';
 import {useState} from "react";
+import Topbar from "./Topbar";
 
-// const useStyles = makeStyles({
-//     table: {
-//         minWidth: 650,
-//     },
-//     chatSection: {
-//         width: '100%',
-//         height: '80vh'
-//     },
-//     headBG: {
-//         backgroundColor: '#e0e0e0'
-//     },
-//     borderRight500: {
-//         borderRight: '1px solid #e0e0e0'
-//     },
-//     messageArea: {
-//         height: '70vh',
-//         overflowY: 'auto'
-//     }
-// });
+import React from 'react';
+
 const users = [
     {
         id: 1,
-        name: 'John Wick',
+        name: 'Ohad Nahari',
         avatar: 'https://material-ui.com/static/images/avatar/1.jpg',
         status: 'online',
     },
     {
         id: 2,
-        name: 'Alice',
+        name: 'Rotem Hashdi',
         avatar: 'https://material-ui.com/static/images/avatar/3.jpg',
         status: 'offline',
     },
     {
         id: 3,
-        name: 'Cindy Baker',
+        name: 'Lia Strauss',
         avatar: 'https://material-ui.com/static/images/avatar/2.jpg',
         status: 'offline',
     },
@@ -60,9 +44,10 @@ export default function ChatBox () {
 
     return (
         <div>
+            <Topbar/>
             <Grid container>
                 <Grid item xs={12}>
-                    <Typography variant="h5" className="header-message">
+                    <Typography variant="h3" className="header-message">
                         Chat
                     </Typography>
                 </Grid>
@@ -70,13 +55,21 @@ export default function ChatBox () {
             <Grid
                 container
                 component={Paper}
-                sx={{ width: '100%', height: '80vh' }}
+                sx={{ width: '100%', height: '80vh'}}
             >
                 <Grid item xs={3} sx={{ borderRight: '1px solid #e0e0e0' }}>
                     <List>
+                        <ListItem key="RemySharp">
+                            <ListItemIcon>
+                                <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="John Wick"></ListItemText>
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <List>
                         {users.map((user) => (
                             <ListItem
-                                button
                                 key={user.id}
                                 onMouseEnter={(event) => handleListItemMouseEnter(event, user.id)}
                                 onMouseLeave={handleListItemMouseLeave}
@@ -94,9 +87,9 @@ export default function ChatBox () {
                         ))}
                     </List>
                     <Divider />
-                    <Grid item xs={12} style={{ padding: '10px' }}>
-                        <TextField id="outlined-basic-email" label="Search" variant="outlined" fullWidth />
-                    </Grid>
+                    {/*<Grid item xs={12} style={{ padding: '10px' }}>*/}
+                    {/*    <TextField id="outlined-basic-email" label="Search" variant="outlined" fullWidth />*/}
+                    {/*</Grid>*/}
                     <Divider />
                 </Grid>
                 <Grid item xs={9}>
