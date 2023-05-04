f8f8a44db179951b9f5c636a15dff05792fc1ea8
yuvalshayo <yuval.shayo@gmail.com>
2023-05-04 16:59:29 +0300

    trying to copy a html chat into mui. gave up

--- src/Components/ChatBox.js
+++ src/Components/ChatBox.js
@@ -6,6 +6,10 @@ import {useState} from "react";
 import Topbar from "./Topbar";
 
 import React from 'react';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
 const users = [
     {
@@ -28,7 +32,7 @@ const users = [
     },
 ];
 
-export default function ChatBox () {
+export function ChatBox1 () {
     const [value, setValue] = useState(null);
     const [hover, setHover] = useState(-1);
 
@@ -238,3 +242,179 @@ export default function ChatBox () {
 //     );
 // }
 
+export default function ChatBox() {
+    return (
+        <div>
+            <Box sx={{ backgroundColor: '#eee' }}>
+                <Box className="container" py={5}>
+                    <Box className="row">
+                        <Box className="col-md-6 col-lg-5 col-xl-4 mb-4 mb-md-0">
+                            <h5 className="font-weight-bold mb-3 text-center text-lg-start">Member</h5>
+                            <Card>
+                                <CardContent>
+                                    <ul className="list-unstyled mb-0">
+                                        <li className="p-2 border-bottom" sx={{ backgroundColor: '#eee' }}>
+                                            <a href="#!" className="d-flex justify-content-between">
+                                                <Box className="d-flex flex-row">
+                                                    <img
+                                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp"
+                                                        alt="avatar"
+                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                        width="60"
+                                                    />
+                                                    <Box className="pt-1">
+                                                        <p className="fw-bold mb-0">John Doe</p>
+                                                        <p className="small text-muted">Hello, Are you there?</p>
+                                                    </Box>
+                                                </Box>
+                                                <Box className="pt-1">
+                                                    <p className="small text-muted mb-1">Just now</p>
+                                                    <span className="badge bg-danger float-end">1</span>
+                                                </Box>
+                                            </a>
+                                        </li>
+                                        <li className="p-2 border-bottom">
+                                            <a href="#!" className="d-flex justify-content-between">
+                                                <Box className="d-flex flex-row">
+                                                    <img
+                                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp"
+                                                        alt="avatar"
+                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                        width="60"
+                                                    />
+                                                    <Box className="pt-1">
+                                                        <p className="fw-bold mb-0">Danny Smith</p>
+                                                        <p className="small text-muted">Lorem ipsum dolor sit.</p>
+                                                    </Box>
+                                                </Box>
+                                                <Box className="pt-1">
+                                                    <p className="small text-muted mb-1">5 mins ago</p>
+                                                </Box>
+                                            </a>
+                                        </li>
+                                        <li className="p-2 border-bottom">
+                                            <a href="#!" className="d-flex justify-content-between">
+                                                <Box className="d-flex flex-row">
+                                                    <img
+                                                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp"
+                                                        alt="avatar"
+                                                        className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                        width="60"
+                                                    />
+                                                    <Box className="pt-1">
+                                                        <p className="fw-bold mb-0">Alex Steward</p>
+                                                        <p className="small text-muted">Lorem ipsum dolor sit.</p>
+                                                    </Box>
+                                                </Box>
+                                                <Box className="pt-1">
+                                                    <p className="small text-muted mb-1">Yesterday</p>
+                                                </Box>
+                                            </a>
+                                        </li>
+                                        <li className="p-2 border-bottom">
+                                            <a href="#!" className="d-flex justify-content-between">
+                                                <Box className="d-flex flex-row">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp" alt="avatar"
+                                                         className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
+                                                    <Box pt={1}>
+                                                        <p className="fw-bold mb-0">John Doe</p>
+                                                        <p className="small text-muted">Hello, Are you there?</p>
+                                                    </Box>
+                                                </Box>
+                                                {/*<Box pt={1}>*/}
+                                                {/*    <p className="small text-muted mb-1">Just now</p>*/}
+                                                {/*    <Badge badgeContent={1} color="error" sx={{ float: 'right' }} />*/}
+                                                {/*</Box>*/}
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </CardContent>
+                            </Card>
+
+                            <Card>
+                                <CardContent sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                                    <Box>
+                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                                             className="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60" />
+                                        <Box pt={1}>
+                                            <p className="fw-bold mb-0">Danny Smith</p>
+                                            <p className="small text-muted">Lorem ipsum dolor sit.</p>
+                                        </Box>
+                                    </Box>
+                                    <CardContent sx={{ pt: 1 }}>
+                                        <Typography variant="subtitle2" color="text.secondary" mb={1}>
+                                            5 mins ago
+                                        </Typography>
+                                    </CardContent>
+                                </CardContent>
+                            </Card>
+                            <Card sx={{ mb: 2 }}>
+                                <Box sx={{ display: "flex", flexDirection: "row" }}>
+                                    <CardMedia
+                                        component="img"
+                                        image="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp"
+                                        alt="avatar"
+                                        sx={{ borderRadius: "50%", width: 60 }}
+                                    />
+                                    <CardContent sx={{ pt: 1 }}>
+                                        <Typography variant="subtitle1" fontWeight="bold" mb={0}>
+                                            Alex Steward
+                                        </Typography>
+                                        <Typography variant="subtitle2" color="text.secondary" sx={{ mb: 1 }}>
+                                            Lorem ipsum dolor sit.
+                                        </Typography>
+                                    </CardContent>
+                                </Box>
+                                <CardContent sx={{ pt: 1 }}>
+                                    <Typography variant="subtitle2" color="text.secondary" mb={1}>
+                                        Yesterday
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            <Card sx={{ mb: 2 }}>
+                                <Box sx={{ display: "flex", flexDirection: "row" }}>
+                                    <CardMedia
+                                        component="img"
+                                        image="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-3.webp"
+                                        alt="avatar"
+                                        sx={{ borderRadius: "50%", width: 60 }}
+                                    />
+                                    <CardContent sx={{ pt: 1 }}>
+                                        <Typography variant="subtitle1" fontWeight="bold" mb={0}>
+                                            Ashley Olsen
+                                        </Typography>
+                                    </CardContent>
+                                </Box>
+                            </Card>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </div>
+
+
+
+
+
+
+
+//
+// {/*                        </Box>*/}
+//             {/*                    </CardContent>*/}
+//             {/*                </Card>*/}
+//             {/*            </Box>*/}
+//             {/*        </Box>*/}
+//             {/*    </Box>*/}
+//             {/*</Box>*/}
+
+// {/*<Card>*/}
+// {/*    <CardContent sx={{ display*/}
+
+
+
+
+
+
+
+                                )
+}
