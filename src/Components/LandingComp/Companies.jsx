0b1de514f12265272bfac6679b0a737308283b37
liastrauss <131149504+liastrauss@users.noreply.github.com>
2023-08-13 16:06:12 +0300

    Update Companies.jsx

--- src/Components/LandingComp/Companies.jsx
+++ src/Components/LandingComp/Companies.jsx
@@ -1,11 +1,8 @@
-import { Box, Container, styled, Typography } from "@mui/material";
+import {Box, Container, styled, Typography} from "@mui/material";
 import React from "react";
-// import logoImg from "../media/logo.png";
-// import starsImg from "../media/Star.png";
-// import logosImg from "../media/logos.png";
 
 const Companies = () => {
-    const CustomContainer = styled(Container)(({ theme }) => ({
+    const CustomContainer = styled(Container)(({theme}) => ({
         display: "flex",
         justifyContent: "space-between",
         [theme.breakpoints.down("md")]: {
@@ -16,14 +13,14 @@ const Companies = () => {
         },
     }));
 
-    const CustomBox = styled(Box)(({ theme }) => ({
+    const CustomBox = styled(Box)(({theme}) => ({
         [theme.breakpoints.down("md")]: {
             marginBottom: theme.spacing(4),
         },
     }));
 
     return (
-        <Box sx={{ mt: 10 }}>
+        <Box sx={{mt: 10}}>
             <CustomContainer>
                 <CustomBox>
                     {/*<img src={logoImg} alt="logo" style={{ maxWidth: "100%" }} />*/}
@@ -56,7 +53,7 @@ const Companies = () => {
                 </Box>
             </CustomContainer>
 
-            <Container sx={{ display: "flex", flexDirection: "column" }}>
+            <Container sx={{display: "flex", flexDirection: "column"}}>
                 {/*<img src={logosImg} alt="logos" />*/}
             </Container>
         </Box>
